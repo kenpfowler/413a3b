ed9f1e4307754822548ad50f5940e84f2ff890ac
Ken <development@kennyf.ca>
2021-12-03 19:29:09 -0500

    configued database, vscode debugger

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,8 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
-  logging: false
-});
+const db = new Sequelize(
+  "postgres://postgres:hatchways@localhost:5432/messenger",
+  { logging: false }
+); // Example for postgres
 
 module.exports = db;
