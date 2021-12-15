c7f4a9420f708d9d4b6d0e73360b7fe1d909cb83
Ken <development@kennyf.ca>
2021-12-14 20:00:14 -0500

    drafted proposed changes to database models

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,17 +1,28 @@
 const Conversation = require("./conversation");
 const User = require("./user");
+// const UserConversatinos = require("./userconversation");
 const Message = require("./message");
 
 // associations
+// Conversation.belongsToMany(User, {
+//   through: UserConversation,
+//   as: "users",
+//   foreignKey: "id",
+// });
+// User.belongsToMany(Conversation, {
+//   through: UserConversation,
+//   foreignKey: "user_id",
+// });
 
-User.hasMany(Conversation);
-Conversation.belongsTo(User, { as: "user1" });
-Conversation.belongsTo(User, { as: "user2" });
+User.hasMany(Conversation); // old assocation to be removed
+Conversation.belongsTo(User, { as: "user1" }); // old assocation to be removed
+Conversation.belongsTo(User, { as: "user2" }); // old assocation to be removed
 Message.belongsTo(Conversation);
 Conversation.hasMany(Message);
 
 module.exports = {
   User,
   Conversation,
-  Message
+  Message,
+  // UserConversations
 };
